7db7a29551ad978739d15e682d4f2561d0ae1ca0
tipikae <gilles.bd@free.fr>
2023-12-09 14:50:15 +0100

    Spring boot project added

---
+++ src/test/java/com/tipikae/chatop/ChaTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipikae.chatop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
