ca15499e8b732714d7ad27caaa225b97d0f9a6e7
tipikae <gilles.bd@free.fr>
2023-12-15 16:33:45 +0100

    Generate token with email added

--- src/main/java/com/tipikae/chatop/services/auth/AuthService.java
+++ src/main/java/com/tipikae/chatop/services/auth/AuthService.java
@@ -1,20 +1,18 @@
 package com.tipikae.chatop.services.auth;
 
+import com.tipikae.chatop.exceptions.AuthenticationException;
 import com.tipikae.chatop.models.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.JwsHeader;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 /**
- * JWT service.
+ * Authentication service.
  */
 @Service
 public class AuthService implements IAuthService {
@@ -26,20 +24,41 @@ public class AuthService implements IAuthService {
      * Generate a token.
      * @param authentication Authentication object.
      * @return Token
+     * @throws AuthenticationException thrown when an authentication exception occurred.
      */
     @Override
-    public Token generateToken(Authentication authentication) {
+    public Token generateTokenWithAuthentication(Authentication authentication) throws AuthenticationException {
+        return generateToken(authentication.getName());
+    }
+
+    /**
+     * Generate a token with email.
+     *
+     * @param email Email.
+     * @return Token
+     * @throws AuthenticationException thrown when an authentication exception occurred.
+     */
+    @Override
+    public Token generateTokenWithEmail(String email) throws AuthenticationException {
+        return generateToken(email);
+    }
+
+    private Token generateToken(String email) throws AuthenticationException {
         Instant now = Instant.now();
         JwtClaimsSet claims =
                 JwtClaimsSet.builder()
-                    .issuer("self")
-                    .issuedAt(now)
-                    .expiresAt(now.plus(1, ChronoUnit.DAYS))
-                    .subject(authentication.getName())
-                    .build();
+                        .issuer("self")
+                        .issuedAt(now)
+                        .expiresAt(now.plus(1, ChronoUnit.DAYS))
+                        .subject(email)
+                        .build();
         JwtEncoderParameters jwtEncoderParameters =
                 JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), claims);
 
-        return new Token(this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue());
+        try {
+            return new Token(this.jwtEncoder.encode(jwtEncoderParameters).getTokenValue());
+        } catch (JwtEncodingException e) {
+            throw new AuthenticationException(e.getMessage());
+        }
     }
 }
--- src/main/java/com/tipikae/chatop/services/auth/IAuthService.java
+++ src/main/java/com/tipikae/chatop/services/auth/IAuthService.java
@@ -1,17 +1,27 @@
 package com.tipikae.chatop.services.auth;
 
+import com.tipikae.chatop.exceptions.AuthenticationException;
 import com.tipikae.chatop.models.Token;
 import org.springframework.security.core.Authentication;
 
 /**
- * JWT service interface.
+ * Authentication service interface.
  */
 public interface IAuthService {
 
     /**
-     * Generate a token.
+     * Generate a token with Authentication.
      * @param authentication Authentication object.
      * @return Token
+     * @throws AuthenticationException thrown when an authentication exception occurred.
      */
-    Token generateToken(Authentication authentication);
+    Token generateTokenWithAuthentication(Authentication authentication) throws AuthenticationException;
+
+    /**
+     * Generate a token with email.
+     * @param email Email.
+     * @return Token
+     * @throws AuthenticationException thrown when an authentication exception occurred.
+     */
+    Token generateTokenWithEmail(String email) throws AuthenticationException;
 }
