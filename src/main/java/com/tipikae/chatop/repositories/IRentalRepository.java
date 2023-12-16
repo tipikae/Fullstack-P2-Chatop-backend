6085a47abdf6a084127f0394ad908b9769f97524
tipikae <gilles.bd@free.fr>
2023-12-16 09:22:40 +0100

    Repository annotation added

--- src/main/java/com/tipikae/chatop/repositories/IRentalRepository.java
+++ src/main/java/com/tipikae/chatop/repositories/IRentalRepository.java
@@ -2,8 +2,10 @@ package com.tipikae.chatop.repositories;
 
 import com.tipikae.chatop.models.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Rental repository.
  */
+@Repository
 public interface IRentalRepository extends JpaRepository<Rental, Long> { }
--- src/main/java/com/tipikae/chatop/repositories/IUserRepository.java
+++ src/main/java/com/tipikae/chatop/repositories/IUserRepository.java
@@ -2,6 +2,7 @@ package com.tipikae.chatop.repositories;
 
 import com.tipikae.chatop.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
 /**
  * User repository.
  */
+@Repository
 public interface IUserRepository extends JpaRepository<User, Long> {
 
     /**
