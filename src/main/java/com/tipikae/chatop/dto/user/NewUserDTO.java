8a66c3b1b11e4b0cd94e521a58682f8bdcf54881
tipikae <gilles.bd@free.fr>
2023-12-16 09:20:24 +0100

    Validation added

--- src/main/java/com/tipikae/chatop/dto/user/NewUserDTO.java
+++ src/main/java/com/tipikae/chatop/dto/user/NewUserDTO.java
@@ -1,5 +1,8 @@
 package com.tipikae.chatop.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +17,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class NewUserDTO {
 
+    @Email(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "Email must have an email format.")
+    @NotBlank(message = "Email must not be blank.")
     private String email;
+
+    @Size(min = 3, message = "Name length must be more than 3 characters.")
+    @NotBlank(message = "Name must not be blank.")
     private String name;
+
+    @Size(min = 3, message = "Password length must be more than 3 characters.")
+    @NotBlank(message = "Password must not be blank.")
     private String password;
 }
