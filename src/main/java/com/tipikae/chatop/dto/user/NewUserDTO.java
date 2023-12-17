65a0a963c1a17d6d16c0f9d9bbc63d668a452870
tipikae <gilles.bd@free.fr>
2023-12-17 09:34:47 +0100

    Size max added

--- src/main/java/com/tipikae/chatop/dto/user/NewUserDTO.java
+++ src/main/java/com/tipikae/chatop/dto/user/NewUserDTO.java
@@ -19,13 +19,14 @@ public class NewUserDTO {
 
     @Email(regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "Email must have an email format.")
     @NotBlank(message = "Email must not be blank.")
+    @Size(max = 255,message = "Email length must be less than 255 characters.")
     private String email;
 
-    @Size(min = 3, message = "Name length must be more than 3 characters.")
+    @Size(min = 3, max = 255,message = "Name length must be between 3 and 255 characters.")
     @NotBlank(message = "Name must not be blank.")
     private String name;
 
-    @Size(min = 3, message = "Password length must be more than 3 characters.")
+    @Size(min = 3, max = 255, message = "Password length must be between 3 and 255 characters.")
     @NotBlank(message = "Password must not be blank.")
     private String password;
 }
