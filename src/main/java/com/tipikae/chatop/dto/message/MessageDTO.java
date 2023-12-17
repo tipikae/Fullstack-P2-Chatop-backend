dce5b8d4f1cb4f4e32dd2c44565a19f1e31663d9
tipikae <gilles.bd@free.fr>
2023-12-17 15:37:30 +0100

    Message DTOs added

---
+++ src/main/java/com/tipikae/chatop/dto/message/MessageDTO.java
@@ -0,0 +1,25 @@
+package com.tipikae.chatop.dto.message;
+
+import com.tipikae.chatop.models.Rental;
+import com.tipikae.chatop.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Message DTO.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDTO {
+
+    private long id;
+    private Rental rental;
+    private User user;
+    private String message;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/tipikae/chatop/dto/message/NewMessageDTO.java
@@ -0,0 +1,27 @@
+package com.tipikae.chatop.dto.message;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * New Message DTO.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewMessageDTO {
+
+    @NotNull(message = "Rental is required.")
+    private long rentalId;
+
+    @NotNull(message = "Sender is required.")
+    private long userId;
+
+    @NotBlank(message = "Message must not be blank.")
+    @Size(max = 2000, message = "Message length must be less than 2000 characters.")
+    private String message;
+}
