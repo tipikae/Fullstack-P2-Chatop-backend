76d770fed5b04e0e8d1a01d3657d440212f87b70
tipikae <gilles.bd@free.fr>
2023-12-17 09:35:02 +0100

    Owner_id and picture removed

--- src/main/java/com/tipikae/chatop/dto/rental/UpdateRentalDTO.java
+++ src/main/java/com/tipikae/chatop/dto/rental/UpdateRentalDTO.java
@@ -2,6 +2,7 @@ package com.tipikae.chatop.dto.rental;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 public class UpdateRentalDTO {
 
     @NotBlank(message = "Name must not be blank.")
+    @Size(max = 255, message = "Name length must be less than 255 characters.")
     private String name;
 
     @NotNull(message = "Surface is required.")
@@ -23,12 +25,7 @@ public class UpdateRentalDTO {
     @NotNull(message = "Price is required.")
     private int price;
 
-    @NotBlank(message = "Picture must not be blank.")
-    private String picture;
-
     @NotBlank(message = "Description must not be blank.")
+    @Size(max = 2000, message = "Description length must be less than 2000 characters.")
     private String description;
-
-    @NotNull(message = "Owner is required.")
-    private long ownerId;
 }
