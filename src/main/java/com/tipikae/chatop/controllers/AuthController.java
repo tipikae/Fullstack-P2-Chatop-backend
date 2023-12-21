38a7f76a5facc4d70cdc85d2288b5274da17805e
tipikae <gilles.bd@free.fr>
2023-12-21 15:19:16 +0100

    Unused imports removed

--- src/main/java/com/tipikae/chatop/controllers/AuthController.java
+++ src/main/java/com/tipikae/chatop/controllers/AuthController.java
@@ -17,8 +17,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
